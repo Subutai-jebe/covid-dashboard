a16311e1a836d21fd6bd350fe9c8ba6ba3350850
k.kwakwamark@gmail.com <caesarion@192.168.1.103>
2023-04-24 20:34:36 +0100

    setup => created secondary sidebar component made with bootstrap

--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -3,12 +3,11 @@ import { CommonModule } from '@angular/common';
 import { MaterialModule } from '@consitency-is-key/material';
 import { CardComponent } from './card/card.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { MainSidebarComponent } from './main-sidebar/main-sidebar.component';
 
 @NgModule({
   imports: [CommonModule, MaterialModule],
-  declarations: [CardComponent, SidebarComponent],
-  exports: [
-    SidebarComponent
-  ]
+  declarations: [CardComponent, SidebarComponent, MainSidebarComponent],
+  exports: [SidebarComponent, MainSidebarComponent],
 })
 export class UiModule {}
