ed432c0d57f592ae8ef3cd482ea23a3092927619
k.kwakwamark@gmail.com <caesarion@192.168.1.103>
2023-04-27 17:03:31 +0100

    setup => setup main component

--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -4,10 +4,16 @@ import { MaterialModule } from '@consitency-is-key/material';
 import { CardComponent } from './card/card.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { MainSidebarComponent } from './main-sidebar/main-sidebar.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   imports: [CommonModule, MaterialModule],
-  declarations: [CardComponent, SidebarComponent, MainSidebarComponent],
-  exports: [SidebarComponent, MainSidebarComponent],
+  declarations: [
+    CardComponent,
+    SidebarComponent,
+    MainSidebarComponent,
+    MainComponent
+  ],
+  exports: [SidebarComponent, MainSidebarComponent, MainComponent],
 })
 export class UiModule {}
