dd92510f20f294d98bee45be40772a579afceab0
k.kwakwamark@gmail.com <caesarion@192.168.1.103>
2023-04-28 14:38:30 +0100

    Created covid dashboard shell application

---
+++ apps/covid-charts/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'consitency-is-key-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'covid-charts';
+}
