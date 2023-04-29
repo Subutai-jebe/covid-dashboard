45493b60887db5b6be5e41fe83231e869702f368
k.kwakwamark@gmail.com <caesarion@192.168.1.103>
2023-04-29 20:20:46 +0100

    created covid-case service to get covid data

---
+++ apps/covid-charts/src/app/dashboard/service/covid-case.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, shareReplay } from 'rxjs';
+import { CountryService } from './country.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CovidCaseService {
+
+  public COVID_DATA_SOURCE$: any;
+
+  constructor(private http: HttpClient,
+    private countryService: CountryService,
+  ) { }
+
+  getWorldSummary(): Observable<any> {
+    return this.http.get(
+      `https://api.covid19api.com/world/total`
+    );
+  }
+
+  setCovidSource(country: string): void {
+    // const sanitisedCountry = this.countryService.sanitize(country);
+    this.COVID_DATA_SOURCE$ = this.http.get(`https://api.covid19api.com/total/country/${country}`).pipe(shareReplay());
+  }
+
+}
