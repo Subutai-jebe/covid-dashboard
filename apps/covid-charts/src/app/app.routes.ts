6e7b493de7db68643b898b9f1d04a37e5d93c10a
k.kwakwamark@gmail.com <caesarion@192.168.1.103>
2023-04-29 15:22:52 +0100

    lazy loaded dashboard module

--- apps/covid-charts/src/app/app.routes.ts
+++ apps/covid-charts/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
+  },
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
