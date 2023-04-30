e9c631aa208fa2addc33a1ab08261d0637926ff4
k.kwakwamark@gmail.com <caesarion@192.168.1.103>
2023-04-30 20:26:03 +0100

    added CovidCountry + LineChartData models

---
+++ apps/covid-charts/src/app/dashboard/models/CovidCountryInfo.ts
@@ -0,0 +1,15 @@
+export interface CovidCountryData {
+  Active: number;
+  City: string;
+  CityCode: string;
+  Comment: string;
+  Confirmed: number;
+  Country: string;
+  CountryCode: string;
+  Date: string;
+  Deaths: number;
+  Lat: string;
+  Lon: string;
+  Province: string;
+  Recovered: string;
+}
